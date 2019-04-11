8e97604735dc147b86799b83c91f88443426bbac
Tae Yeon Yoo <taeyeon_yoo@hms.harvard.edu>
2019-04-11 17:44:21 -0400

    error corrected

--- measureNucleiProperties.py
+++ measureNucleiProperties.py
@@ -66,11 +66,11 @@ def normalize_image(im,low=None,high=None):
     return im    
 
 def subtractBg(im,method='min'):
-    if method='mode':
+    if method=='mode':
         filtered = skimage.filters.gaussian(im,sigma=2)
         h = np.histogram(filtered,bins=500);
         bg = h[1][h[0].argmax()]
-    elif method='min':
+    elif method=='min':
         bg = im.flatten().min()
     im[im<bg] = bg
     im = im-bg
